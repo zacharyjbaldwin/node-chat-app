ee925da9b99ca87a88cbadd30aba96b87a440666
Zachary Baldwin <zacharyjbaldwin@gmail.com>
2017-11-05 21:09:40 -0600

    First commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '../public');
+
+var app = express();
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`Express server listening on port ${port}!`);
+});
